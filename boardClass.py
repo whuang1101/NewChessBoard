7bcedaf00718917701f09a2d7f36b810d6710f41
Wilson Huang <wilsonhuang11012000@gmail.com>
2023-04-18 23:38:49 -0400

    finally correct

--- boardClass.py
+++ boardClass.py
@@ -2,51 +2,74 @@ import pygame
 
 
 class Piece:
-    def __init__(self, color):
-        self.name = None
+    def __init__(self, color, row, column):
+        self.name = " "
         self.color = color
+        self.row = row
+        self.column = column
+
+    def move_piece(self, row, column):
+        self.row = row
+        self.column = column
 
 
 class Pawn(Piece):
-    def __init__(self, color):
-        super().__init__(color)
+    def __init__(self, color, row, column):
+        super().__init__(color, row, column)
         self.name = "pawn"
+        self.original_row = self.row
+        self.original_column = self.column
+
+    def possible_moves(self, board):
+        moves = []
+        if self.color == "white" and (self.row == self.original_row and self.column == self.original_column):
+            if board[self.row][self.column] == board[self.row-1][self.column]:
+                move = [self.row-1,self.column]
+                moves.append(move)
+                if board[self.row][self.column] == board[self.row - 2][self.column]:
+                    move = [self.row - 2, self.column]
+                    moves.append(move)
+        elif self.color == "white":
+            if board[self.row][self.column] == board[self.row-1][self.column]:
+                move = [self.row-1, self.column]
+                moves.append(move)
+        return moves
 
 
 class Rook(Piece):
-    def __init__(self, color):
-        super().__init__(color)
+    def __init__(self, color, row, column):
+        super().__init__(color, row, column)
         self.name = "rook"
 
 
 class Knight(Piece):
-    def __init__(self, color):
-        super().__init__(color)
+    def __init__(self, color, row, column):
+        super().__init__(color, row, column)
         self.name = "knight"
 
 
 class Bishop(Piece):
-    def __init__(self, color):
-        super().__init__(color)
+    def __init__(self, color, row, column):
+        super().__init__(color, row, column)
         self.name = "bishop"
 
 
 class Queen(Piece):
-    def __init__(self, color):
-        super().__init__(color)
+    def __init__(self, color, row, column):
+        super().__init__(color, row, column)
         self.name = "queen"
 
 
 class King(Piece):
-    def __init__(self, color):
-        super().__init__(color)
+    def __init__(self, color, row, column):
+        super().__init__(color, row, column)
         self.name = "king"
 
 
 class Board:
     class Cell:
         def __init__(self):
-            self.piece = Piece("none")
+            self.piece = Piece(" ", 0, 0)
             self.background_color = " "
             self.x = 0
             self.y = 0
@@ -62,25 +85,25 @@ class Board:
         for i in range(1, 7, 5):
             for j in range(0, 8):
                 if i == 1:
-                    self.board[j][i].piece = Pawn("black")
+                    self.board[i][j].piece = Pawn("black", j, j)
                 if i == 6:
-                    self.board[j][i].piece = Pawn("white")
-        self.board[0][0].piece = Rook("black")
-        self.board[1][0].piece = Bishop("black")
-        self.board[2][0].piece = Knight("black")
-        self.board[3][0].piece = Queen("black")
-        self.board[4][0].piece = King("black")
-        self.board[5][0].piece = Knight("black")
-        self.board[6][0].piece = Bishop("black")
-        self.board[7][0].piece = Rook("black")
-        self.board[0][7].piece = Rook("white")
-        self.board[1][7].piece = Bishop("white")
-        self.board[2][7].piece = Knight("white")
-        self.board[3][7].piece = Queen("white")
-        self.board[4][7].piece = King("white")
-        self.board[5][7].piece = Knight("white")
-        self.board[6][7].piece = Bishop("white")
-        self.board[7][7].piece = Rook("white")
+                    self.board[i][j].piece = Pawn("white", i, j)
+        self.board[0][0].piece = Rook("black", 0, 0)
+        self.board[0][1].piece = Bishop("black", 1, 0)
+        self.board[0][2].piece = Knight("black", 2, 0)
+        self.board[0][3].piece = Queen("black", 3, 0)
+        self.board[0][4].piece = King("black", 4, 0)
+        self.board[0][5].piece = Knight("black", 5, 0)
+        self.board[0][6].piece = Bishop("black", 6, 0)
+        self.board[0][7].piece = Rook("black", 7, 0)
+        self.board[7][0].piece = Rook("white", 0, 7)
+        self.board[7][1].piece = Bishop("white", 1, 7)
+        self.board[7][2].piece = Knight("white", 2, 7)
+        self.board[7][3].piece = Queen("white", 3, 7)
+        self.board[7][4].piece = King("white", 4, 7)
+        self.board[7][5].piece = Knight("white", 5, 7)
+        self.board[7][6].piece = Bishop("white", 6, 7)
+        self.board[7][7].piece = Rook("white", 7, 7)
         for i in range(0, 8):
             for j in range(0, 8):
                 if i % 2 == 0:
@@ -107,8 +130,11 @@ class Board:
     def set_player(self, a):  # makes it so that it is either player one or 2;
         self.player = a
 
+    def get_board(self):
+        return self.board
+
     def print_board(self):
         for i in range(len(self.board)):
             for j in range(len(self.board[0])):
-                print(self.board[i][j].piece, end=" ")
+                print(self.board[i][j].piece.name, end=" ")
             print()
--- main.py
+++ main.py
@@ -10,23 +10,36 @@ Y = 560
 
 # initializing white chess images
 white_pawn = pygame.image.load("wp.png")
+white_pawn = pygame.transform.rotate(white_pawn, 270)
 white_rook = pygame.image.load("wR.png")
+white_rook = pygame.transform.rotate(white_rook, 270)
 white_bishop = pygame.image.load("wB.png")
+white_bishop = pygame.transform.rotate(white_bishop, 270)
 white_knight = pygame.image.load("wN.png")
+white_knight = pygame.transform.rotate(white_knight, 270)
 white_queen = pygame.image.load("wQ.png")
+white_queen = pygame.transform.rotate(white_queen, 270)
 white_king = pygame.image.load("wK.png")
-
+white_king = pygame.transform.rotate(white_king, 270)
 black_pawn = pygame.image.load("bp.png")
+black_pawn = pygame.transform.rotate(black_pawn, 270)
 black_rook = pygame.image.load("bR.png")
+black_rook = pygame.transform.rotate(black_rook, 270)
 black_bishop = pygame.image.load("bB.png")
+black_bishop = pygame.transform.rotate(black_bishop, 270)
 black_knight = pygame.image.load("bN.png")
+black_knight = pygame.transform.rotate(black_knight, 270)
 black_queen = pygame.image.load("bQ.png")
+black_queen = pygame.transform.rotate(black_queen, 270)
 black_king = pygame.image.load("bK.png")
+black_king = pygame.transform.rotate(black_king, 270)
 
 
 chess_display = pygame.display.set_mode((X, Y))
 chessBoard = Board()
+chessBoard.print_board()
 while True:
+    current_board = chessBoard.get_board()
     pygame.display.set_caption("Chess Game")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -38,8 +51,12 @@ while True:
                     mouse_x, mouse_y = pygame.mouse.get_pos()
                     if((i + 1) * 70 >= mouse_y >= i * 70) and (j * 70 <= mouse_x <= (j + 1) * 70):
                         print(chr(97+i), j+1)
+                        print(chessBoard.board[i][j].piece.name)
+                        if chessBoard.board[i][j].piece.name != " ":
+                            moves = chessBoard.board[i][j].piece.possible_moves(current_board)
 
-        # this prints all the pieces on the board
+#
+#         # this prints all the pieces on the board
         for i in range(0, 8):
             for j in range(0, 8):
                 piece_rect = white_pawn.get_rect()
@@ -77,6 +94,11 @@ while True:
                     chess_display.blit(black_queen, piece_rect)
                 elif chessBoard.board[i][j].piece.name == "king" and chessBoard.board[i][j].piece.color == "black":
                     chess_display.blit(black_king, piece_rect)
+
+        rotated_surface = pygame.Surface(chess_display.get_size(), pygame.SRCALPHA)
+        rotated_surface.blit(chess_display, (0, 0))
+        rotated_surface = pygame.transform.rotate(rotated_surface, 90)
+        chess_display.blit(rotated_surface, (0, 0))
 
         pygame.display.flip()
 
