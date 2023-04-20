9c2e365f3c1ebc698be68de1c6bf0fd7c85e6838
Wilson Huang <wilsonhuang11012000@gmail.com>
2023-04-20 18:56:40 -0400

    kinda working

--- boardClass.py
+++ boardClass.py
@@ -1,64 +1,4 @@
-import pygame
-
-
-class Piece:
-    def __init__(self, color):
-        self.color = color
-        self.name = "--"
-
-
-class Pawn(Piece):
-    def __init__(self, color):
-        super().__init__(color)
-        if self.color == "white":
-            self.name = "wp"
-        else:
-            self.name = "bp"
-
-
-class Rook(Piece):
-    def __init__(self, color):
-        super().__init__(color)
-        if self.color == "white":
-            self.name = "wR"
-        else:
-            self.name = "bR"
-
-
-class Bishop(Piece):
-    def __init__(self, color):
-        super().__init__(color)
-        if self.color == "white":
-            self.name = "wB"
-        else:
-            self.name = "bB"
-
-
-class Knight(Piece):
-    def __init__(self, color):
-        super().__init__(color)
-        if self.color == "white":
-            self.name = "wN"
-        else:
-            self.name = "bN"
-
-
-class Queen(Piece):
-    def __init__(self, color):
-        super().__init__(color)
-        if self.color == "white":
-            self.name = "wQ"
-        else:
-            self.name = "bQ"
-
-
-class King(Piece):
-    def __init__(self, color):
-        super().__init__(color)
-        if self.color == "white":
-            self.name = "wK"
-        else:
-            self.name = "bK"
+from pieceClass import Piece, Queen, King, Pawn, Rook, Bishop, Knight
 
 
 class Board:
@@ -69,6 +9,7 @@ class Board:
             self.col = col
 
     def __init__(self):
+        self.status = []
         self.board = []
         for row in range(8):
             add = []
@@ -105,88 +46,9 @@ class Board:
                 print(self.board[row][col].piece.name, end=" ")
             print()
 
-        else:
-            self.name = "bR"
+    def get_board(self):
+        return self.board
 
-
-class Bishop(Piece):
-    def __init__(self, color):
-        super().__init__(color)
-        if self.color == "white":
-            self.name = "wB"
-        else:
-            self.name = "bB"
-
-
-class Knight(Piece):
-    def __init__(self, color):
-        super().__init__(color)
-        if self.color == "white":
-            self.name = "wN"
-        else:
-            self.name = "bN"
-
-
-class Queen(Piece):
-    def __init__(self, color):
-        super().__init__(color)
-        if self.color == "white":
-            self.name = "wQ"
-        else:
-            self.name = "bQ"
-
-
-class King(Piece):
-    def __init__(self, color):
-        super().__init__(color)
-        if self.color == "white":
-            self.name = "wK"
-        else:
-            self.name = "bK"
-
-
-class Board:
-    class Cell:
-        def __init__(self, row, col):
-            self.piece = Piece("white")
-            self.row = row
-            self.col = col
-
-    def __init__(self):
-        self.board = []
-        for row in range(8):
-            add = []
-            for col in range(8):
-                add.append(Board.Cell(row, col))
-
-            self.board.append(add)
-        for row in range(8):
-            for col in range(8):
-                if row == 1:
-                    self.board[row][col].piece = Pawn("black")
-                elif row == 6:
-                    self.board[row][col].piece = Pawn("white")
-        self.board[0][0].piece = Rook("black")
-        self.board[0][1].piece = Bishop("black")
-        self.board[0][2].piece = Knight("black")
-        self.board[0][3].piece = Queen("black")
-        self.board[0][4].piece = King("black")
-        self.board[0][5].piece = Knight("black")
-        self.board[0][6].piece = Bishop("black")
-        self.board[0][7].piece = Rook("black")
-        self.board[7][0].piece = Rook("white")
-        self.board[7][1].piece = Bishop("white")
-        self.board[7][2].piece = Knight("white")
-        self.board[7][3].piece = Queen("white")
-        self.board[7][4].piece = King("white")
-        self.board[7][5].piece = Knight("white")
-        self.board[7][6].piece = Bishop("white")
-        self.board[7][7].piece = Rook("white")
-
-    def print_board(self):
-        for row in range(8):
-            for col in range(8):
-                print(self.board[row][col].piece.name, end=" ")
-            print()
-
-            
+    def update_status(self,current_board):
+        # create a new instance of Board
+        self.status.append(current_board)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import boardClass
 from boardClass import Board
 import pygame
-
+import copy
 
 square_size = 64
 board_dimension = 512
@@ -32,6 +32,8 @@ def draw_pieces(display, board):
                 display.blit(images[board[row][col].piece.name],
                              pygame.Rect(col * square_size, row * square_size, square_size, square_size))
 
+# row and column need to be switched for any type of printing to board
+
 
 if __name__ == "__main__":
     chess_board = Board()
@@ -39,10 +41,50 @@ if __name__ == "__main__":
     load_images()
     chess_display = pygame.display.set_mode((board_dimension, board_dimension))
     chess_open = True
+    yellow_size = (64, 64)
+    yellow_surface = pygame.Surface(yellow_size, pygame.SRCALPHA)
+    yellow_surface.fill((255, 255, 0, 128))
+    frames = pygame.time.Clock()
+    moves = []
+    player = 1
+    previous_click = []
     while chess_open:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 chess_open = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # have to use cols, rows because of the way pygame starts at the top left instead of the bottom right.
+                col, row = pygame.mouse.get_pos()
+                row = int(row/ square_size)
+                col = int(col/square_size)
+                if len(moves) != 0:
+                    for i, j in moves:
+                        if i == row and j == col:
+                            chess_board.board[previous_click[0][0]][previous_click[0][1]].piece.make_move(chess_board.board, i, j, previous_click)
+                            current_Board = copy.deepcopy(chess_board)
+                            chess_board.update_status(current_Board)
+                            player += 1
+                    moves.clear()
+                elif chess_board.board[row][col].piece.name != "--":
+                    previous_click.clear()
+                    if player % 2 == 1 and chess_board.board[row][col].piece.name[0] == "w":
+                        moves = chess_board.board[row][col].piece.possible_moves(chess_board.board, row, col)
+                    if player % 2 == 0 and chess_board.board[row][col].piece.name[0] == "b":
+                        moves = chess_board.board[row][col].piece.possible_moves(chess_board.board, row, col)
+
+                    print(chr(97+int(col)), 8-int(row))
+                    previous_click.append([row, col])
         draw_squares(chess_display)
         draw_pieces(chess_display, chess_board.board)
+
+        if len(moves) != 0:
+            for col,row in moves:
+                yellow_squares = (row*64, col*64)
+                chess_display.blit(yellow_surface, yellow_squares)
+
+        pygame.display.update()
+        frames.tick(20)
         pygame.display.flip()
+
+for replay in chess_board.status:
+    replay.print_board()
