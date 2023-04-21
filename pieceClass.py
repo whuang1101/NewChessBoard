9ebce4986ed33e44fe0caab53f3d017317171466
Wilson Huang <wilsonhuang11012000@gmail.com>
2023-04-21 01:06:41 -0400

    all pieces move yay
    :

--- pieceClass.py
+++ pieceClass.py
@@ -235,6 +235,65 @@ class Knight(Piece):
         else:
             self.name = "bN"
 
+    def possible_moves(self,board, row, column):
+        moves = []
+        if self.color == "white":
+            if row + 2 < 8 and column + 1 < 8:
+                if board[row+2][column+1].piece.name == "--" or board[row+2][column + 1].piece.color[0] == "b":
+                    moves.append((row+2, column+1))
+            if row+2 < 8 and column - 1 >= 0:
+                if board[row+2][column-1].piece.name == "--" or board[row +2][column - 1].piece.color[0] == "b":
+                    moves.append((row+2, column-1))
+            if row - 2 >= 0 and column - 1 >= 0:
+                if board[row - 2][column - 1].piece.name == "--" or board[row - 2][column - 1].piece.color[0] == "b":
+                    moves.append((row - 2, column - 1))
+            if row - 2 >= 0 and column + 1 < 8:
+                if board[row - 2][column + 1].piece.name == "--" or board[row - 2][column + 1].piece.color[0] == "b":
+                    moves.append((row - 2, column + 1))
+            if row + 1 < 8 and column + 2 < 8:
+                if board[row+1][column+2].piece.name == "--" or board[row+1][column + 2].piece.color[0] == "b":
+                    moves.append((row+1, column+2))
+            if row+1 < 8 and column - 2 >= 0:
+                if board[row+1][column-2].piece.name == "--" or board[row +1][column - 2].piece.color[0] == "b":
+                    moves.append((row+1, column-2))
+            if row - 1 >= 0 and column - 2 >= 0:
+                if board[row - 1][column - 2].piece.name == "--" or board[row - 1][column - 2].piece.color[0] == "b":
+                    moves.append((row - 1, column - 2))
+            if row - 1 >= 0 and column + 2 < 8:
+                if board[row - 1][column + 2].piece.name == "--" or board[row - 1][column + 2].piece.color[0] == "b":
+                    moves.append((row - 1, column + 2))
+        else:
+            if row + 2 < 8 and column + 1 < 8:
+                if board[row+2][column+1].piece.name == "--" or board[row+2][column + 1].piece.color[0] == "w":
+                    moves.append((row+2, column+1))
+            if row+2 < 8 and column - 1 >= 0:
+                if board[row+2][column-1].piece.name == "--" or board[row +2][column - 1].piece.color[0] == "w":
+                    moves.append((row+2, column-1))
+            if row - 2 >= 0 and column - 1 >= 0:
+                if board[row - 2][column - 1].piece.name == "--" or board[row - 2][column - 1].piece.color[0] == "w":
+                    moves.append((row - 2, column - 1))
+            if row - 2 >= 0 and column + 1 < 8:
+                if board[row - 2][column + 1].piece.name == "--" or board[row - 2][column + 1].piece.color[0] == "w":
+                    moves.append((row - 2, column + 1))
+            if row + 1 < 8 and column + 2 < 8:
+                if board[row+1][column+2].piece.name == "--" or board[row+1][column + 2].piece.color[0] == "w":
+                    moves.append((row+1, column+2))
+            if row+1 < 8 and column - 2 >= 0:
+                if board[row+1][column-2].piece.name == "--" or board[row +1][column - 2].piece.color[0] == "w":
+                    moves.append((row+1, column-2))
+            if row - 1 >= 0 and column - 2 >= 0:
+                if board[row - 1][column - 2].piece.name == "--" or board[row - 1][column - 2].piece.color[0] == "w":
+                    moves.append((row - 1, column - 2))
+            if row - 1 >= 0 and column + 2 < 8:
+                if board[row - 1][column + 2].piece.name == "--" or board[row - 1][column + 2].piece.color[0] == "w":
+                    moves.append((row - 1, column + 2))
+        return moves
+
+    @staticmethod
+    def make_move(board, row, col, previous):
+        board[row][col].piece = board[previous[0][0]][previous[0][1]].piece
+        board[previous[0][0]][previous[0][1]].piece = Piece()
+
 
 class Queen(Piece):
     def __init__(self, color):
@@ -243,6 +302,19 @@ class Queen(Piece):
             self.name = "wQ"
         else:
             self.name = "bQ"
+    def possible_moves(self,board, row, column):
+        moves = []
+
+    def possible_moves(self, board, row, column):
+        rook_moves = Rook.possible_moves(self, board, row, column)
+        bishop_moves = Bishop.possible_moves(self, board, row, column)
+        moves = rook_moves + bishop_moves
+        return moves
+
+    @staticmethod
+    def make_move(board, row, col, previous):
+        board[row][col].piece = board[previous[0][0]][previous[0][1]].piece
+        board[previous[0][0]][previous[0][1]].piece = Piece()
 
 
 class King(Piece):
@@ -252,3 +324,23 @@ class King(Piece):
             self.name = "wK"
         else:
             self.name = "bK"
+
+    def possible_moves(self, board, row, column):
+        moves = []
+        for i in range(row - 1, row+2):
+            for j in range(column -1, column +2):
+                if i < 0 or i>= 8 or j< 0 or j >= 8:
+                    continue
+                else:
+                    if board[row][column].piece.color == "white":
+                        if board[i][j].piece.name == "--" or board[i][j].piece.name[0] == "b":
+                            moves.append((i, j))
+                    else:
+                        if board[i][j].piece.name == "--" or board[i][j].piece.name[0] == "w":
+                            moves.append((i, j))
+        return moves
+
+    @staticmethod
+    def make_move(board, row, col, previous):
+        board[row][col].piece = board[previous[0][0]][previous[0][1]].piece
+        board[previous[0][0]][previous[0][1]].piece = Piece()
